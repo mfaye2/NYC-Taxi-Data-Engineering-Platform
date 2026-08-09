75c00203b6dcf72034e7f6e364353ae06a49873d
tyrlafaye <fayelatyr61@gmail.com>
2026-08-09 11:04:44 +0200

    mise a jour du fichier de dag pour ajouter wait (glue , curated, rejected )

--- airflow/dags/nyc_taxi_pipeline.py
+++ airflow/dags/nyc_taxi_pipeline.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
+import time
 import subprocess
+import boto3
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -13,6 +15,130 @@ GLUE_JOB_NAME = "nyc-taxi-data-platform-dev-transform-trips"
 CURATED_CRAWLER = "nyc-taxi-data-platform-dev-curated-trips-crawler"
 REJECTED_CRAWLER = "nyc-taxi-data-platform-dev-rejected-trips-crawler"
 
+
+# ---------------------------------------------------------
+# AWS CLIENT
+# ---------------------------------------------------------
+
+def get_glue_client():
+    session = boto3.Session(
+        profile_name=AWS_PROFILE,
+        region_name=AWS_REGION
+    )
+
+    return session.client("glue")
+
+
+# ---------------------------------------------------------
+# GLUE JOB
+# ---------------------------------------------------------
+
+def start_glue_job(**context):
+    glue = get_glue_client()
+
+    response = glue.start_job_run(
+        JobName=GLUE_JOB_NAME
+    )
+
+    job_run_id = response["JobRunId"]
+
+    print(f"Glue JobRunId: {job_run_id}")
+
+    context["ti"].xcom_push(
+        key="glue_job_run_id",
+        value=job_run_id
+    )
+
+
+def wait_for_glue_job(**context):
+    glue = get_glue_client()
+
+    job_run_id = context["ti"].xcom_pull(
+        task_ids="start_glue_job",
+        key="glue_job_run_id"
+    )
+
+    while True:
+        response = glue.get_job_run(
+            JobName=GLUE_JOB_NAME,
+            RunId=job_run_id,
+            PredecessorsIncluded=False
+        )
+
+        state = response["JobRun"]["JobRunState"]
+
+        print(f"Glue status: {state}")
+
+        if state == "SUCCEEDED":
+            print("Glue job terminé avec succès.")
+            break
+
+        if state in [
+            "FAILED",
+            "STOPPED",
+            "TIMEOUT",
+            "ERROR",
+            "EXPIRED"
+        ]:
+            raise RuntimeError(
+                f"Glue job terminé avec l'état : {state}"
+            )
+
+        time.sleep(30)
+
+
+# ---------------------------------------------------------
+# CRAWLERS
+# ---------------------------------------------------------
+
+def start_crawler(crawler_name):
+    glue = get_glue_client()
+
+    glue.start_crawler(
+        Name=crawler_name
+    )
+
+    print(f"Crawler démarré : {crawler_name}")
+
+
+def wait_for_crawler(crawler_name):
+    glue = get_glue_client()
+
+    while True:
+        response = glue.get_crawler(
+            Name=crawler_name
+        )
+
+        state = response["Crawler"]["State"]
+
+        print(f"{crawler_name} status: {state}")
+
+        if state == "READY":
+            print(f"{crawler_name} terminé.")
+            break
+
+        time.sleep(20)
+
+
+def start_curated_crawler():
+    start_crawler(CURATED_CRAWLER)
+
+
+def wait_curated_crawler():
+    wait_for_crawler(CURATED_CRAWLER)
+
+
+def start_rejected_crawler():
+    start_crawler(REJECTED_CRAWLER)
+
+
+def wait_rejected_crawler():
+    wait_for_crawler(REJECTED_CRAWLER)
+
+
+# ---------------------------------------------------------
+# DBT
+# ---------------------------------------------------------
 
 def run_command(command: str):
     print(f"Commande exécutée : {command}")
@@ -31,70 +157,27 @@ def run_command(command: str):
 
     if result.returncode != 0:
         raise RuntimeError(
-            f"La commande a échoué avec le code {result.returncode}"
+            f"Commande échouée avec le code {result.returncode}"
         )
 
 
-import boto3
-
-def get_glue_client():
-    session = boto3.Session(
-        profile_name="nyc-taxi-dev",
-        region_name="eu-north-1"
-    )
-
-    return session.client("glue")
-
-
-def start_glue_job():
-    glue = get_glue_client()
-
-    response = glue.start_job_run(
-        JobName=GLUE_JOB_NAME
-    )
-
-    return response["JobRunId"]
-
-
-def run_curated_crawler():
-    command = (
-        f"aws glue start-crawler "
-        f'--name "{CURATED_CRAWLER}" '
-        f"--profile {AWS_PROFILE} "
-        f"--region {AWS_REGION}"
-    )
-
-    run_command(command)
-
-
-def run_rejected_crawler():
-    command = (
-        f"aws glue start-crawler "
-        f'--name "{REJECTED_CRAWLER}" '
-        f"--profile {AWS_PROFILE} "
-        f"--region {AWS_REGION}"
-    )
-
-    run_command(command)
-
-
 def run_dbt():
-    command = (
+    run_command(
         "cd /opt/project/dbt && "
-        "dbt run --profiles-dir ."
+        "dbt run --no-partial-parse --profiles-dir ."
     )
-
-    run_command(command)
 
 
 def test_dbt():
-    command = (
+    run_command(
         "cd /opt/project/dbt && "
         "dbt test --profiles-dir ."
     )
 
-    run_command(command)
 
+# ---------------------------------------------------------
+# AIRFLOW CONFIG
+# ---------------------------------------------------------
 
 default_args = {
     "owner": "data-engineering",
@@ -119,14 +202,29 @@ with DAG(
         python_callable=start_glue_job,
     )
 
+    wait_glue = PythonOperator(
+        task_id="wait_for_glue_job",
+        python_callable=wait_for_glue_job,
+    )
+
     curated_crawler = PythonOperator(
-        task_id="run_curated_crawler",
-        python_callable=run_curated_crawler,
+        task_id="start_curated_crawler",
+        python_callable=start_curated_crawler,
+    )
+
+    wait_curated = PythonOperator(
+        task_id="wait_curated_crawler",
+        python_callable=wait_curated_crawler,
     )
 
     rejected_crawler = PythonOperator(
-        task_id="run_rejected_crawler",
-        python_callable=run_rejected_crawler,
+        task_id="start_rejected_crawler",
+        python_callable=start_rejected_crawler,
+    )
+
+    wait_rejected = PythonOperator(
+        task_id="wait_rejected_crawler",
+        python_callable=wait_rejected_crawler,
     )
 
     dbt_run = PythonOperator(
@@ -139,4 +237,14 @@ with DAG(
         python_callable=test_dbt,
     )
 
-    start_glue >> curated_crawler >> rejected_crawler >> dbt_run >> dbt_test
+
+    (
+        start_glue
+        >> wait_glue
+        >> curated_crawler
+        >> wait_curated
+        >> rejected_crawler
+        >> wait_rejected
+        >> dbt_run
+        >> dbt_test
+    )
